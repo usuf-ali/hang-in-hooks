59114aef0c2f15cec44b9b1dcdfcaf27f4885ddb
Usuf Ali <12yfali12@gmail.com>
2022-11-09 16:42:26 +0600

    counter 2 added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import { useReducer } from 'react';
 import './App.css';
 
-const initVal = { counter: 1 };
+const initVal = {
+  counter: 1,
+  counter2: 0
+};
 const reducer = (state, action) => {
   switch (action.type) {
     case 'add':
-      return { counter: state.counter + 1 }
+      return { ...state, counter: state.counter + 1 }
     case 'sub':
-      return { counter: state.counter - 1 }
+      return { ...state, counter: state.counter - 1 }
+    case 'add2':
+      return { ...state, counter2: state.counter2 + 2 }
+    case 'sub2':
+      return { ...state, counter2: state.counter2 - 2 }
     default:
       return state;
   }
 };
-
 function App() {
   const [state, dispatch] = useReducer(reducer, initVal);
-
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => dispatch({ type: 'add' })}>+</button>
-        <h1>{state.counter}</h1>
-        <button onClick={() => dispatch({ type: 'sub' })}>-</button>
+        <div className="counter">
+          <div className="btn">
+            <button onClick={() => dispatch({ type: 'add' })}>+</button>
+            <h1>{state.counter}</h1>
+            <button onClick={() => dispatch({ type: 'sub' })}>-</button>
+          </div>
+          <div className="btn">
+            <button onClick={() => dispatch({ type: 'add2' })}>+</button>
+            <h2>{state.counter2}</h2>
+            <button onClick={() => dispatch({ type: 'sub2' })}>-</button>
+          </div>
+        </div>
         <h2>Hello World</h2>
       </header>
     </div>
